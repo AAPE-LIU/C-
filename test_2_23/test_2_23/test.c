fb8da453f4f417b1143b1da5dbd20f9a590acbd6
AAPE-LIU <1369578672@qq.com>
2021-02-23 22:20:52 +0800

    函数

---
+++ test_2_23/test_2_23/test.c
@@ -0,0 +1,16 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+# include <stdio.h>
+//函数的用法
+//int sum(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 25;
+//	int add = sum(a,b);
+//	printf("%d\n",add);
+//	return 0;
+//}
